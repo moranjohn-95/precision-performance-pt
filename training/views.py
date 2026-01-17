d6502a4ddb78bf0b5f5dd98d5c7c693f8d7df724
moranjohn-95 <moranjohn95@gmail.com>
2026-01-17 17:44:08 +0000

    Add new template with inital strucutre for booking consultation page

--- training/views.py
+++ training/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Create views here.
+def consultation_request(request):
+
+    return render(request, "training/consultation.html")
