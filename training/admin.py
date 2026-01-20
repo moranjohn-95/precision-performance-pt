6028ed6ba57f78470864b26482597d2489bb33c6
moranjohn-95 <moranjohn95@gmail.com>
2026-01-20 14:37:33 +0000

    Register ConsultationRequest in Django admin with useful list display

--- training/admin.py
+++ training/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import ConsultationRequest
 
-# Register your models here.
+
+@admin.register(ConsultationRequest)
+class ConsultationRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "training_goal",
+        "coaching_option",
+        "status",
+        "assigned_trainer",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "coaching_option",
+        "training_goal",
+        "preferred_date",
+        "assigned_trainer",
+        "created_at",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+    )
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at", "updated_at")
