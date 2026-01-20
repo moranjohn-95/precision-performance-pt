01acf419f93390e77ff4ae9c124dc53127407aca
moranjohn-95 <moranjohn95@gmail.com>
2026-01-20 16:10:59 +0000

    Include accounts URLs in main project routing

--- precision_performance/urls.py
+++ precision_performance/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from training import views as training_views
 
@@ -56,4 +56,7 @@ urlpatterns = [
         training_views.consultation,
         name="consultation_request",
     ),
+
+    # Accounts (login routes)
+    path("", include("accounts.urls")),
 ]
