23f00292d2475775a245164cf58524b0bd870b5f
moranjohn-95 <moranjohn95@gmail.com>
2026-01-20 15:56:36 +0000

    Add initial trainer and client login pages to views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
+# accounts/views.py
 from django.shortcuts import render
 
-# Create your views here.
+
+def trainer_login(request):
+    """
+    Placeholder trainer login page for now.
+    """
+    return render(request, "accounts/trainer_login.html")
+
+
+def client_login(request):
+    """
+    Placeholder client login page for now.
+    """
+    return render(request, "accounts/client_login.html")
