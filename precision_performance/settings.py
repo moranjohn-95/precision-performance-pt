df2ef00ced0fb25c7f051e857ea928987aae2bc3
moranjohn-95 <moranjohn95@gmail.com>
2026-01-21 16:08:06 +0000

    Fix logout issue to ensure user is returned to home page

--- precision_performance/settings.py
+++ precision_performance/settings.py
@@ -118,6 +118,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Redirect here after logout
+LOGOUT_REDIRECT_URL = "home"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
