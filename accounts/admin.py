5baf4f674d7ade534e667c44f066ffe7a016c6ad
moranjohn-95 <moranjohn95@gmail.com>
2026-01-22 20:39:32 +0000

    Register Clientprofile in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ClientProfile
+
+
+@admin.register(ClientProfile)
+class ClientProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "preferred_trainer",
+        "consultation_request",
+        "created_at",
+    )
+    list_filter = ("preferred_trainer", "created_at")
+    search_fields = (
+        "user__username",
+        "user__email",
+        "consultation_request__email",
+        "consultation_request__last_name",
+    )
+    readonly_fields = ("created_at", "updated_at")
