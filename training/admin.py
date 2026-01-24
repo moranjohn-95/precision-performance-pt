81e2fbc6479d8a7b8a0cef32c8894bdb7d2572ba
moranjohn-95 <moranjohn95@gmail.com>
2026-01-24 16:14:48 +0000

    Register workout session, set and body metic models

--- training/admin.py
+++ training/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import ConsultationRequest
+from .models import (
+    ConsultationRequest,
+    WorkoutSession,
+    WorkoutSet,
+    BodyMetricEntry,
+)
 
 
 @admin.register(ConsultationRequest)
@@ -29,3 +34,40 @@ class ConsultationRequestAdmin(admin.ModelAdmin):
     )
     ordering = ("-created_at",)
     readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(WorkoutSession)
+class WorkoutSessionAdmin(admin.ModelAdmin):
+    list_display = ("client", "date", "name", "created_at")
+    list_filter = ("date", "client")
+    search_fields = ("name", "client__email", "client__username")
+    ordering = ("-date", "-created_at")
+
+
+@admin.register(WorkoutSet)
+class WorkoutSetAdmin(admin.ModelAdmin):
+    list_display = (
+        "session",
+        "exercise_name",
+        "set_number",
+        "reps",
+        "weight_kg",
+        "rpe",
+    )
+    list_filter = ("exercise_name", "session__client")
+    search_fields = ("exercise_name", "session__name")
+
+
+@admin.register(BodyMetricEntry)
+class BodyMetricEntryAdmin(admin.ModelAdmin):
+    list_display = (
+        "client",
+        "date",
+        "bodyweight_kg",
+        "waist_cm",
+        "bench_top_set_kg",
+        "sleep_hours",
+    )
+    list_filter = ("date", "client")
+    search_fields = ("client__email", "client__username")
+    ordering = ("-date", "-created_at")
